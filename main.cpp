e217df70c799b963cb39fc43b8bc548dcbdc2f8b
hectorrami <hramirez4@uh.edu>
2019-11-30 14:29:02 -0600

    "compresses message without eol correctly"

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
 #include <pthread.h>
+#include <iostream>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stack>
 using namespace std;
@@ -11,10 +10,10 @@ static pthread_mutex_t sem;
 static pthread_cond_t myturn = PTHREAD_COND_INITIALIZER;
 
 struct data{
+    string str;
+    string temporary;
     int n[100];
-    char symbol;
     string message[100];
-    string Decompressed;
 };
 
 void *child_thread(void *arg){
@@ -28,49 +27,47 @@ void *child_thread(void *arg){
     while(x != turn)
         pthread_cond_wait(&myturn, &sem);
     //cout << "I am thread #" << x << endl;
-    
-    
-    string temp = pos2->message[current];
+    string temp;
     string msg;
-    char symbol = temp[0];
-
-
-    cout << "Symbol : " << symbol << endl;
-    cout << "Binary : ";
+    char symbol;
+    //cout << "Message here: " << pos2->str << endl;
+    temp = pos2->message[current];
+    symbol = temp[0];
+    //cout << "Symbol : " << symbol << endl;
     for(int i =2 ; i< temp.size(); i++)
         msg += temp[i];
-
-    cout << msg << endl;
-
-    /*If statement to insert if there is a 0 at that position*/
-   
-
+    // cout << "Binary : ";
+    // cout << msg << endl;
     pthread_mutex_unlock(&sem);
-
-
 
     pthread_mutex_lock(&sem);
     //cout << "Thread #" << turn << " ended" << endl;
-
-
     for(int i =0; i < msg.size(); i++){
         if(msg[i] == '1')
             msg[i] = symbol;
     }
 
-    cout << "MSG: " << msg << endl;
+   //cout << "MSG: " << msg << endl;
+   for(int i =0 ; i < pos2->str.size(); i++){
+       
+       for(int j =0 ; j < msg.size(); j++)
+            if(msg[j] == '0'){
+               msg[j] = pos2->str[i];
+               i++;
+            }
+
+   }
+    pos2->str = msg;
 
     string str1 = pos2->message[current];
     string str2 = " Binary code = ";
     str1.insert(1,str2);
+    cout << str1 << endl;
 
-
-    //cout << str1 << endl;
     current++;
     turn++;
     pthread_cond_broadcast(&myturn);
     pthread_mutex_unlock(&sem);
-
     return NULL;
 }
 
@@ -84,11 +81,11 @@ int main(){
         str_stack.push(input);
     }
    
-    int NTHREADS = 5;
+    int NTHREADS = str_stack.size();
     pthread_t tid[NTHREADS];
     pthread_mutex_init(&sem, NULL); 
 
-    for(int i =0; i < 5;++i)
+    for(int i =0; i < NTHREADS;++i)
     {
         pos.n[i] = i+1;
         pos.message[i] = str_stack.top();
@@ -101,11 +98,12 @@ int main(){
     }
 
     /*Wait for the other threads to finish*/
-    for(int i =0; i < 5; i++)
+    for(int i =0; i < NTHREADS; i++)
         pthread_join(tid[i], NULL);
 
-    cout << "Decompressed file components: " << endl;
-    //cout << pos.Decompressed << endl;
+    /*prinT out the final decompressed message*/
+    cout << "Decompressed file contents:" << endl;
+    cout << pos.str << endl;
 
     return 0; 
 }
